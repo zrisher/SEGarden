0b9adc73e3477f0a5dd1161e7cd8b90a47d01446
Zach Risher <zach.risher@gmail.com>
2015-09-22 12:30:26 -0700

    working on getting pilots

---
+++ Extensions/Cockpit.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sandbox.ModAPI;
+using Ingame = Sandbox.ModAPI.Ingame;
+
+using VRage.ModAPI;
+using VRage.Components;
+
+namespace SEGarden.Extensions {
+
+    using Logging;
+
+    public static class CockpitExtensions {
+
+        private static Logger Log = new Logger("SEGarden.Extensions.CockpitExtensions");
+
+
+        public static IMyCharacter GetPilot(this Ingame.IMyCockpit cockpit) {
+            var children = new List<IMyEntity>();
+            cockpit.GetChildren(children, (e) => e is IMyCharacter);
+
+            if (children.Count > 0) {
+                return children.First() as IMyCharacter;
+            }
+
+            return null;          
+        }
+
+    }
+
+}
