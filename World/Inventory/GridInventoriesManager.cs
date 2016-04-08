230ae175c2cae94437989c8d639fc58c1584bd0c
Zach Risher <zach.risher@gmail.com>
2016-04-08 00:23:57 -0700

    inventory mgmt

---
+++ World/Inventory/GridInventoriesManager.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sandbox.Definitions; // from Sandbox.Game.dll
+using Sandbox.Game.Entities; // from Sandbox.Game.dll
+using Sandbox.ModAPI;
+
+using VRage; // from VRage.dll and VRage.Library.dll
+using VRage.Game; // from VRage.Game.dll
+using VRage.Game.Entity; // from VRage.Game.dll
+using VRage.Game.ModAPI; // from VRage.Game.dll
+using VRage.ModAPI; // from VRage.Game.dll
+using VRage.ObjectBuilders;
+
+using SEGarden.Logging;
+
+namespace SEGarden.World.Inventory {
+    
+    /// <summary>
+    /// Tracks the counts for a configurable set of items across a 
+    /// configurable set of blocks on a given grid.
+    /// </summary>
+    /// <remarks>
+    /// TODO: Filter found inventories by a configurable subtypename
+    /// </remarks>
+    class GridInventoriesManager {
+
+        public InventoryItemsCount Totals;
+
+        readonly Dictionary<MyInventoryBase, InventoryItemsCount> InventoryTotals;
+        readonly List<MyDefinitionId> WatchedItems;
+        readonly Logger Log;
+        readonly IMyCubeGrid Grid;
+
+        public GridInventoriesManager(IMyCubeGrid grid, List<MyDefinitionId> watchedItems = null) {
+            Grid = grid;
+            Log = new Logger("SEGarden.World.Inventory.GridInventoriesManager", (() => Grid.EntityId.ToString()));
+            Totals = new InventoryItemsCount();
+            InventoryTotals = new Dictionary<MyInventoryBase, InventoryItemsCount>();
+            WatchedItems = watchedItems; 
+        }
+
+        public void Initialize() {
+            Grid.OnBlockAdded += BlockAdded;
+            Grid.OnBlockRemoved += BlockRemoved;
+
+            List<IMySlimBlock> allBlocks = new List<IMySlimBlock>();
+            Grid.GetBlocks(allBlocks);
+            foreach (var block in allBlocks) {
+                BlockAdded(block);
+            }
+        }
+
+        public void Terminate() {
+            Grid.OnBlockAdded -= BlockAdded;
+            Grid.OnBlockRemoved -= BlockRemoved;
+
+            foreach (var inventory in InventoryTotals.Keys) {
+                inventory.ContentsChanged -= UpdateInventory;
+            }
+        }
+
+        private void BlockAdded(IMySlimBlock slimblock) {
+            //Log.Trace(slimblock.ToString() + " added to InventoryManager for " + Grid.DisplayName, "blockAdded");
+
+            if (slimblock == null) {
+                Log.Error("Received null slimblock", "blockAdded");
+                return;
+            }
+                
+            MyEntity fatEntity = slimblock.FatBlock as MyEntity;
+            if (fatEntity == null) return;
+
+            MyInventoryBase inventory;
+            if (!fatEntity.TryGetInventory(out inventory)) return;
+
+            Log.Trace("Adding inventory " + slimblock.ToString(), "blockAdded");
+            InventoryTotals[inventory] = new InventoryItemsCount();
+            UpdateInventory(inventory);
+            inventory.ContentsChanged += UpdateInventory;
+        }
+
+        private void BlockRemoved(IMySlimBlock slimblock) {
+            //Log.Trace(slimblock.ToString() + " removed from InventoryManager for " + Grid.DisplayName, "blockRemoved");
+
+            if (slimblock == null) {
+                Log.Error("Received null slimblock", "blockRemoved");
+                return;
+            }
+
+            MyEntity fatEntity = slimblock.FatBlock as MyEntity;
+            if (fatEntity == null)
+                return;
+
+            MyInventoryBase inventory;
+            if (!fatEntity.TryGetInventory(out inventory))
+                return;
+
+            Log.Trace("Removing inventory " + slimblock.ToString(), "blockRemoved");
+            if (!InventoryTotals.Remove(inventory)) {
+                Log.Error("Received an removal for inventory we're not tracking.", "blockRemoved");
+                return;
+            }
+            inventory.ContentsChanged -= UpdateInventory;
+        }
+
+        private void UpdateInventory(MyInventoryBase inventory) {
+            Log.Trace("Updating inventory cache with inventory " + inventory.Entity.ToString(), "UpdateInventory");
+            InventoryItemsCount itemCache;
+            if (!InventoryTotals.TryGetValue(inventory, out itemCache)) {
+                Log.Error("Received an update for inventory we're not tracking.", "UpdateInventory");
+                return;
+            }
+
+            Totals -= itemCache;
+
+            if (WatchedItems != null) {
+                foreach (var id in WatchedItems) {
+                    itemCache.Set(id, inventory.GetItemAmount(id));
+                }
+            }
+            else {
+                foreach (var item in inventory.GetItems()) {
+                    itemCache.Set(item.Content.GetObjectId(), item.Amount);
+                }
+            }
+
+            Totals += itemCache;
+
+            DebugPrint();
+        }
+
+        private void DebugPrint() {
+            Log.Debug("Displaying inventory contents for grid: " + Grid.DisplayName, "DebugInventories");
+
+            foreach (var kvp in Totals.GetCounts()) {
+                Log.Debug("    " + kvp.Key.ToString() + " - " + kvp.Value, "DebugInventories");
+            }
+
+            /*
+            foreach (var kvp in Counts) {
+                Log.Debug("  contents for block: " + kvp.Key.Entity.ToString(), "DebugInventories");
+
+                foreach (var def in WatchedItems) {
+                    Log.Debug("    " + def.DisplayNameText + " - " + kvp.Value.Get(def.Id), "DebugInventories");
+                }
+            }
+             * */
+        }
+
+    }
+
+}
---
+++ World/Inventory/InventoryItemsCount.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Sandbox.Definitions; // from Sandbox.Game.dll
+using Sandbox.Game.Entities; // from Sandbox.Game.dll
+using Sandbox.ModAPI;
+
+using VRage; // from VRage.dll and VRage.Library.dll
+using VRage.Game; // from VRage.Game.dll
+using VRage.Game.Entity; // from VRage.Game.dll
+using VRage.Game.ModAPI; // from VRage.Game.dll
+using VRage.ModAPI; // from VRage.Game.dll
+using VRage.ObjectBuilders;
+
+namespace SEGarden.World.Inventory {
+
+    /// <summary>
+    /// Represents a collection of item counts as a vector
+    /// </summary>
+    public class InventoryItemsCount {
+
+        public static InventoryItemsCount Zero = new InventoryItemsCount();
+
+        #region Operators
+
+        public static InventoryItemsCount operator +(InventoryItemsCount value1, InventoryItemsCount value2) {
+            VRage.Exceptions.ThrowIf<ArgumentNullException>(value1 == null, "value1");
+            VRage.Exceptions.ThrowIf<ArgumentNullException>(value2 == null, "value2");
+
+            InventoryItemsCount result = value1.Copy();
+
+            foreach (var kvp in value2.Counts) {
+                result.Counts[kvp.Key] = result.Get(kvp.Key) + kvp.Value;
+            }
+
+            return result;
+        }
+
+        public static InventoryItemsCount operator -(InventoryItemsCount value1, InventoryItemsCount value2) {
+            VRage.Exceptions.ThrowIf<ArgumentNullException>(value1 == null, "value1");
+            VRage.Exceptions.ThrowIf<ArgumentNullException>(value2 == null, "value2");
+
+            InventoryItemsCount result = value1.Copy();
+
+            foreach (var kvp in value2.Counts) {
+                result.Counts[kvp.Key] = result.Get(kvp.Key) - kvp.Value;
+            }
+
+            return result;
+        }
+
+        public static InventoryItemsCount operator *(InventoryItemsCount value1, MyFixedPoint value2) {
+            VRage.Exceptions.ThrowIf<ArgumentNullException>(value1 == null, "value1");
+            VRage.Exceptions.ThrowIf<ArgumentNullException>(value2 == null, "value2");
+
+            InventoryItemsCount result = value1.Copy();
+
+            foreach (var kvp in result.Counts) {
+                result.Counts[kvp.Key] *= value2;
+            }
+
+            return result;
+        }
+
+        /*
+        public static bool operator <(InventoryItemsCount value1, InventoryItemsCount value2) {
+            return value2.Contains(value1);
+        }
+
+        public static bool operator <=(InventoryItemsCount value1, InventoryItemsCount value2) {
+            return value1 < value2;
+        }
+        */
+
+        #endregion
+
+        protected Dictionary<MyDefinitionId, MyFixedPoint> Counts =
+            new Dictionary<MyDefinitionId, MyFixedPoint>();
+
+        /*
+        public void SetCount(MyDefinitionId defId, MyFixedPoint count) {
+            Counts[defId] = count;
+        }
+
+        public MyFixedPoint GetCount(MyDefinitionId defId) {
+            return Counts[defId];
+        }
+        */
+
+        public MyFixedPoint Get(MyDefinitionId definitionId) {
+            return Counts.ContainsKey(definitionId) ? Counts[definitionId] : MyFixedPoint.Zero;
+        }
+
+        public void Set(MyDefinitionId defId, MyFixedPoint count) {
+            Counts[defId] = count;
+        }
+
+        public bool IsEmpty() {
+            return Counts.Values.All((x) => (x <= MyFixedPoint.Zero));
+        }
+
+        public bool Contains(InventoryItemsCount other) {
+            return other.Counts.All((kvp) => (kvp.Value <= Get(kvp.Key)));
+        }
+
+        public Dictionary<MyDefinitionId, MyFixedPoint> GetCounts() {
+            VRage.Exceptions.ThrowIf<FieldAccessException>(this.Counts == null, "this.Counts");
+
+            return new Dictionary<MyDefinitionId, MyFixedPoint>(Counts);
+        }
+
+        protected InventoryItemsCount Copy() {
+            InventoryItemsCount result = new InventoryItemsCount();
+            result.Counts = this.GetCounts();
+            return result;
+        }
+
+        public String ToString() {
+            String result = "[ ";
+            foreach (var kvp in Counts) {
+                result += kvp.Key.ToString() + " " + kvp.Value.ToString() + ", ";
+            }
+
+            return result + " ]";
+        }
+
+    }
+}
